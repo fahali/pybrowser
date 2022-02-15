34dfdce184aea4b10d54b018d348d7d4a7ef43f6
fahali <fahim.ali.204@gmail.com>
2022-02-14 23:14:40 -0500

    F# : Browser Engineering - Chapter 1
    add support for https and specified ports

--- browser.py
+++ browser.py
@@ -1,27 +1,38 @@
 def request(url):
     import socket
+    import ssl
 
+    HTTP_SCHEME = "http"
+    HTTPS_SCHEME = "https"
     HTTP_PROTOCOL_1_0 = "HTTP/1.0"
-    HTTP_PREFIX = "http://"
-    DEFAULT_PATH = "/index.html"
     METHOD_GET = "GET"
     STATUS_OK = "200"
 
     NEWLINE = "\r\n"
-    EMPTY_STRING = ""
     PATH_SEPARATOR = "/"
+    SCHEME_SEPARATOR = "://"
+    PORT_SEPARATOR = ":"
     HEADER_SEPARATOR = " "
     HEADER_KEY_VALUE_SEPARATOR = ":"
 
     HTTP_PORT = 80
+    HTTPS_PORT = 443
     ENCODING = "utf8"
     READONLY = "r"
 
-    assert url.startswith(HTTP_PREFIX)  # confirm HTTP protocol
-    url = url[len(HTTP_PREFIX) :]  # discard the prefix
+    scheme, url = url.split(SCHEME_SEPARATOR, 1)
+    assert scheme in [HTTP_SCHEME, HTTPS_SCHEME], f"Unknown scheme {scheme}"
+
+    # set a default port to start
+    port = HTTP_PORT if scheme == HTTP_SCHEME else HTTPS_PORT
 
     host, path = url.split(PATH_SEPARATOR, 1)
     path = f"/{path}"  # replace the stripped path separator
+
+    # get the correct port if it's available
+    if PORT_SEPARATOR in host:
+        host, port = host.split(PORT_SEPARATOR, 1)
+        port = int(port)
 
     request_action = f"{METHOD_GET} /index.html {HTTP_PROTOCOL_1_0}{NEWLINE}"
     request_header = f"Host: {host}{NEWLINE * 2}"
@@ -30,7 +41,11 @@ def request(url):
     tcp_socket = socket.socket(
         family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
     )
-    tcp_socket.connect((host, HTTP_PORT))
+    tcp_socket.connect((host, port))
+
+    if scheme == HTTPS_SCHEME:
+        context = ssl.create_default_context()
+        tcp_socket = context.wrap_socket(tcp_socket, server_hostname=host)
 
     tcp_socket.send(request)
     response = tcp_socket.makefile(READONLY, encoding=ENCODING, newline=NEWLINE)
